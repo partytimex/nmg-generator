fbd601eee21c18930b76fc02c75ff3202c2d7fd5
partytimex <partytimex@hotmail.com>
2014-04-09 16:38:45 +0800

    Update test-load.js

--- test/test-load.js
+++ test/test-load.js
@@ -2,7 +2,7 @@
 'use strict';
 var assert = require('assert');
 
-describe('f2e generator', function () {
+describe('nmg generator', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
     assert(app !== undefined);
